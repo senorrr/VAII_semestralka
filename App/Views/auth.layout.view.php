ecc95cbcd1c39cafd70723a14e21aebed259e225
senorrr <117277359+senorrr@users.noreply.github.com>
2024-11-28 14:55:17 +0100

    nahratie Frameworku Vaiicko

---
+++ App/Views/auth.layout.view.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var string $contentHTML */
+?>
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <title><?= \App\Config\Configuration::APP_NAME ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="public/css/styl.css">
+    <script src="public/js/script.js"></script>
+</head>
+<body>
+<div class="container-fluid mt-3">
+    <div class="web-content">
+        <?= $contentHTML ?>
+    </div>
+</div>
+</body>
+</html>
