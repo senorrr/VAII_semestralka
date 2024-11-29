a7132da0ea8a11de81e5571daafe6df529628765
senorr <macko.didko512@gmail.com>
2024-11-29 18:45:46 +0100

    nahratie exportu z databazy

---
+++ docker/sql/users.sql
@@ -0,0 +1,25 @@
+-- Adminer 4.8.1 MySQL 11.6.2-MariaDB-ubu2404 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+CREATE DATABASE `vaiicko_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+USE `vaiicko_db`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `surname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+INSERT INTO `users` (`email`, `password`, `name`, `surname`) VALUES
+('admin@gmail.com',	'admin',	'Admin',	'Adminec'),
+('marek@gmail.com',	'123',	'Marek',	'Figo');
+
+-- 2024-11-29 17:42:35
