ec5a461957239e8bd30999a7ac358c596559c9a1
senorrr <117277359+senorrr@users.noreply.github.com>
2024-12-03 22:17:08 +0100

    pridanie skriptu na tabulku

---
+++ docker/sql/adverts.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `advert`;
+CREATE TABLE `advert` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `dateOfCreate` date NOT NULL,
+                          `timeOfLastEdit` datetime NOT NULL,
+                          `text` varchar(1000) NOT NULL,
+                          `title` varchar(100) NOT NULL,
+                          `owner` varchar(100) NOT NULL,
+                          `categoryId` int(11) NOT NULL,
+                          `categoryName` varchar(100) NOT NULL,
+                          `city` varchar(100) NOT NULL,
+                          `reviewId` int(11) NOT NULL,
+                          `availabilityId` int(11) NOT NULL,
+                          `price` double NOT NULL,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci COMMENT='cudzie kluce: caregoryId, reviewId, availibilityId';
+
