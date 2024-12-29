79b908da840585223b25d9535e9b8b417b70fe24
senorr <macko.didko512@gmail.com>
2024-12-29 11:20:03 +0100

    kontrola ze ci existuju udaje

--- App/Views/Advert/add.view.php
+++ App/Views/Advert/add.view.php
@@ -74,10 +74,12 @@ use App\Models\Category;
             <?php
                 if (sizeof($data) > 9) {
                     for ($i = 1; $i <= sizeof($data)-9; $i++) {
-                        echo '<div class="col url-input d-flex align-items-center">';
-                        echo '<input type="url" name="photo' . $i . '" class="form-control" placeholder="Zadajte url adresu ' . $i . '. fotky" value="' . $data['photo'.$i] . '">';
-                        echo '<button class="vymaz-but" onclick="removeField(this)">X</button>';
-                        echo '</div>';
+                        if (isset($data[$i])) {
+                            echo '<div class="col url-input d-flex align-items-center">';
+                            echo '<input type="url" name="photo' . $i . '" class="form-control" placeholder="Zadajte url adresu ' . $i . '. fotky" value="' . $data['photo'.$i] . '">';
+                            echo '<button class="vymaz-but" onclick="removeField(this)">X</button>';
+                            echo '</div>';
+                        }
                     }
                 }
             ?>
